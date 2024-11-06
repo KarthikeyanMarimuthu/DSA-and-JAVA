b106809e36cacf9f4a987c64268d60e378fc196a
Karthikeyan M (Cons) <Karthikeyan.M@mobileum.com>
2024-11-06 23:52:14 +0530

    Oct 28/24 Adavanced (module 1) Assignments and Add Problems (Bit Manipulation 2)

---
+++ DSA/src/main/java/com/advanced/module1/assignments/Problem15.java
@@ -0,0 +1,94 @@
+package com.advanced.module1.assignments;
+
+/*
+Q1. Single Number II
+        Solved
+feature icon
+Using hints except Complete Solution is Penalty free now
+Use Hint
+Problem Description
+Given an array of integers, every element appears thrice except for one, which occurs once.
+
+Find that element that does not appear thrice.
+
+NOTE: Your algorithm should have a linear runtime complexity.
+
+Could you implement it without using extra memory?
+
+
+
+
+Problem Constraints
+2 <= A <= 5*106
+
+        0 <= A <= INTMAX
+
+
+
+Input Format
+First and only argument of input contains an integer array A.
+
+
+
+        Output Format
+Return a single integer.
+
+
+
+Example Input
+Input 1:
+
+A = [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+Input 2:
+
+A = [0, 0, 0, 1]
+
+
+Example Output
+Output 1:
+
+        4
+Output 2:
+
+        1
+
+
+Example Explanation
+Explanation 1:
+
+        4 occurs exactly once in Input 1.
+        1 occurs exactly once in Input 2.
+*/
+
+
+public class Problem15 {
+
+    public int singleNumber(final int[] A) {
+
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < A.length; j++) {
+
+
+                if ((A[j] & (1 << i)) > 0) {
+
+                    count++;
+                }
+
+
+            }
+
+            if (count % 3 != 0) {
+
+                res = res + (1 << i);
+            }
+
+        }
+        return res;
+    }
+
+
+}
---
+++ DSA/src/main/java/com/advanced/module1/assignments/Problem16.java
@@ -0,0 +1,100 @@
+package com.advanced.module1.assignments;
+
+/*
+Q2. Single Number III
+        Solved
+feature icon
+Using hints except Complete Solution is Penalty free now
+Use Hint
+Problem Description
+Given an array of positive integers A, two integers appear only once, and all the other integers appear twice.
+Find the two integers that appear only once.
+
+Note: Return the two numbers in ascending order.
+
+
+        Problem Constraints
+2 <= |A| <= 100000
+        1 <= A[i] <= 109
+
+
+
+Input Format
+The first argument is an array of integers of size N.
+
+
+
+        Output Format
+Return an array of two integers that appear only once.
+
+
+
+Example Input
+Input 1:
+A = [1, 2, 3, 1, 2, 4]
+Input 2:
+
+A = [1, 2]
+
+
+Example Output
+Output 1:
+        [3, 4]
+Output 2:
+
+        [1, 2]
+
+
+Example Explanation
+Explanation 1:
+        3 and 4 appear only once.
+        Explanation 2:
+
+        1 and 2 appear only once.
+
+*/
+
+
+import java.util.Arrays;
+
+public class Problem16 {
+
+    public int[] solve(int[] A) {
+
+        int xor_value = A[0];
+
+        for(int i =1 ; i< A.length; i++){
+
+            xor_value = xor_value ^ A[i];
+        }
+
+        int pos = 0;
+        for(int i =0 ; i < 32 ; i++){
+
+            if((xor_value & (1 << i)) > 0){
+
+                pos = i;
+                break;
+            }
+        }
+
+        int res[] = new int[2];
+
+        for(int i =0; i< A.length; i++){
+
+            if((A[i] & (1 << pos)) > 0){
+
+                res[0] = res[0] ^ A[i];
+            }
+
+            else{
+                res[1] = res[1] ^ A[i];
+            }
+        }
+
+        Arrays.sort(res);
+
+        return res;
+
+    }
+}
